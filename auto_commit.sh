7fd2b19522c6d5a5d91d8c38d823e9de15c65598
IchinaZiru <yutaihuangjing08@gmail.com>
2025-08-29 16:41:44 +0900

    Update auto_commit.sh

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,8 +3,10 @@
 # ステージングをすべてクリア
 git reset
 
-# v2 ディレクトリだけの変更ファイルを取得
-files=$(git ls-files --modified --others --exclude-standard)
+# 変更されたdata配下のファイルを取得
+files=$(git ls-files --modified --others --exclude-standard | grep '^outputs/')
+# 注意: このスクリプトは 全てのディレクトリ内の変更ファイルを対象としています。
+#files=$(git ls-files --modified --others --exclude-standard)
 
 # 1ファイルずつ add → commit
 for file in $files
